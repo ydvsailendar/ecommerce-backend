512de8e5cd27c6c8d46baf3602f9e88939fa7913
ydvsailendar <ydvsailendar0905@gmail.com>
2020-10-01 09:42:16 +0545

    graphql server

---
+++ server.js
@@ -0,0 +1,25 @@
+const { GraphQLServer } = require('graphql-yoga');
+
+const dinnerOptions = ['🍕', '🌭', '🍔', '🥗', '🍣'];
+
+const typeDefs = `
+  type Query {
+    whatsForDinner: String!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    whatsForDinner: () => {
+      const idx = Math.floor(Math.random() * dinnerOptions.length);
+      const foodChoice = dinnerOptions[idx];
+      return `Tonight we eat ${foodChoice}`;
+    }
+  }
+};
+
+const server = new GraphQLServer({ typeDefs, resolvers });
+
+server.start(() => {
+  console.log(`😄 Server running at http://localhost:4000`);
+});
